e1a9527e6b6f5ff9df0c60442bc17c2fd5145614
Takuma HANATANI <mail@potato4d.me>
2022-05-07 19:16:21 +0900

    chore: use terser for compress

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,14 @@ import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    minify: 'terser',
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "LIFF ScreeenShot Plugin",
       fileName: (format) => `index.${format}.js`,
+    },
+    terserOptions: {
+      mangle: true
     },
     rollupOptions: {
       external: ['@line/liff'],
